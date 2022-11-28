eb59dbbb66bea5ec57eb8c77cb888b7cdb861137
mai refaat <mairefaat2019@gmail.com>
2022-11-28 14:04:49 +0200

    create project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,255 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img
+        src="..\images\MicrosoftTeams-image (7).png"
+        className="img-fluid"
+        alt="Responsive image"
+      ></img>
+      {/* section 1 */}
+      <div className="section1">
+        <div className="asset">
+          <h2>
+            <span>Asset Tracker</span>
+          </h2>
+        </div>
+        <div className="text">
+          <p>
+            Asset Tracker is an Android App integrated with ivanti Neurons for
+            ITSM and ITAM to track active & passive assets and assist Ivanti
+            systems to keep assets data accurate by adding and modifying assets
+            on the go and streamline assets audits and save more than 80% of the
+            manual assets audit.
+          </p>
+        </div>
+
+        {/* card */}
+
+        <div class="row">
+          <div class="col-sm-6">
+            <div class="card">
+              <div class="card-body">
+                <iframe
+                  src="https://player.vimeo.com/video/720586774?autoplay=1&loop=1"
+                  frameborder="0"
+                  allow="autoplay; fullscreen; encrypted-media"
+                  data-lf-form-tracking-inspected-9k315xmg7aodlb0m="true"
+                  data-lf-yt-playback-inspected-9k315xmg7aodlb0m="true"
+                  data-lf-vimeo-playback-inspected-9k315xmg7aodlb0m="true"
+                ></iframe>
+              </div>
+
+
+              <div className="card-text">
+            <h3>
+              <span>Asset Tracker by TjDeeD</span>
+            </h3>
+            <p>English version</p>
+          </div>
+
+          
+            </div>
+          </div>
+          <div class="col-sm-6">
+            <div class="card">
+              <div class="card-body">
+                <iframe
+                  src="https://player.vimeo.com/video/720586774?autoplay=1&loop=1"
+                  frameborder="0"
+                  allow="autoplay; fullscreen; encrypted-media"
+                  data-lf-form-tracking-inspected-9k315xmg7aodlb0m="true"
+                  data-lf-yt-playback-inspected-9k315xmg7aodlb0m="true"
+                  data-lf-vimeo-playback-inspected-9k315xmg7aodlb0m="true"
+                ></iframe>
+              </div>
+
+              <div className="card-text">
+            <h3>
+              <span>Asset Tracker by TjDeeD</span>
+            </h3>
+            <p>Arabic version</p>
+          </div>
+            </div>
+          </div>
+        </div>
+        {/* <!--/.Card--> */}
+
+        {/* <div className="card-text">
+          <div>
+            <h3>
+              <span>Asset Tracker by TjDeeD</span>
+            </h3>
+            <p>English version</p>
+          </div>
+
+          <div>
+            <h3>
+              <span>Asset Tracker by TjDeeD</span>
+            </h3>
+            <p>Arabic version</p>
+          </div>
+        </div> */}
+      </div>
+      {/* section2 */}
+      <div className="section2 ">
+        <div className="asset1">
+          <h2>
+            <span>Outlook Plugin</span>
+          </h2>
+        </div>
+        <div className="text">
+          <p>
+            Outlook Plugin add-on for ivanti Neurons for ITSM solution is an
+            office plugin installed in outlook client on windows devices, it
+            automatically uses your email content to do actions in ivanti
+            Neurons for ITSM. Whether it’s a new incident, new task, adding a
+            note to either an incident or a task or even creating a service
+            request filling its fields by email body, subject or attachment. A
+            simple way to save time and effort with a click from your outlook
+            and keep ITSM system rich with data like conversation with endusers
+            and vendors.
+          </p>
+        </div>
+
+        {/* card */}
+
+        <div class="row">
+          <div class="col-sm-6">
+            <div class="card">
+              <div class="card-body">
+                <iframe
+                  src="https://player.vimeo.com/video/720586338?autoplay=1&loop=1"
+                  frameborder="0"
+                  allow="autoplay; fullscreen; encrypted-media"
+                  data-lf-form-tracking-inspected-9k315xmg7aodlb0m="true"
+                  data-lf-yt-playback-inspected-9k315xmg7aodlb0m="true"
+                  data-lf-vimeo-playback-inspected-9k315xmg7aodlb0m="true"
+                ></iframe>
+              </div>
+
+              <div className="card-text">
+          <div>
+            <h3>
+              <span>Outlook Plugin by TjDeeD</span>
+            </h3>
+            <p>English version</p>
+          </div>
+          </div>
+
+            </div>
+          </div>
+          <div class="col-sm-6">
+            <div class="card">
+              <div class="card-body">
+                <iframe
+                  src="https://player.vimeo.com/video/720586210?autoplay=1&loop=1"
+                  frameborder="0"
+                  allow="autoplay; fullscreen; encrypted-media"
+                  data-lf-form-tracking-inspected-9k315xmg7aodlb0m="true"
+                  data-lf-yt-playback-inspected-9k315xmg7aodlb0m="true"
+                  data-lf-vimeo-playback-inspected-9k315xmg7aodlb0m="true"
+                ></iframe>
+              </div>
+
+              <div className="card-text">
+          <div>
+            <h3>
+              <span>Outlook Plugin by TjDeeD</span>
+            </h3>
+            <p>Arabic version</p>
+          </div>
+        </div>
+
+            </div>
+          </div>
+        </div>
+        {/* <!--/.Card--> */}
+
+      </div>
+
+      {/* section3 */}
+      <div className="section3 ">
+        <div className="asset3">
+          <h2>
+            <span>Arabic Localisation</span>
+          </h2>
+        </div>
+        <div className="text">
+          <p>
+            Arabic localization is a custom package designed for Middle East
+            ivanti customers to arabize the end-user’s interface in ivanti
+            Neurons for ITSM and ITAM.
+          </p>
+        </div>
+
+        {/* card */}
+
+        <div class="row">
+          <div class="col-sm-6">
+            <div class="card">
+              <div class="card-body">
+                <iframe
+                  src="https://player.vimeo.com/video/720586476?autoplay=1&loop=1"
+                  frameborder="0"
+                  allow="autoplay; fullscreen; encrypted-media"
+                  data-lf-form-tracking-inspected-9k315xmg7aodlb0m="true"
+                  data-lf-yt-playback-inspected-9k315xmg7aodlb0m="true"
+                  data-lf-vimeo-playback-inspected-9k315xmg7aodlb0m="true"
+                ></iframe>
+              </div>
+
+
+              <div className="card-text">
+          <div>
+            <h3>
+              <span>ITSM Localisation by TjDeeD</span>
+            </h3>
+            <p>English version</p>
+          </div>
+          </div>
+
+
+            </div>
+          </div>
+          <div class="col-sm-6">
+            <div class="card">
+              <div class="card-body">
+                <iframe
+                  src="https://player.vimeo.com/video/720586627?autoplay=1&loop=1"
+                  frameborder="0"
+                  allow="autoplay; fullscreen; encrypted-media"
+                  data-lf-form-tracking-inspected-9k315xmg7aodlb0m="true"
+                  data-lf-yt-playback-inspected-9k315xmg7aodlb0m="true"
+                  data-lf-vimeo-playback-inspected-9k315xmg7aodlb0m="true"
+                ></iframe>
+              </div>
+
+              <div className="card-text">
+          <div>
+            <h3>
+              <span>ITSM Localisation by TjDeeD</span>
+            </h3>
+            <p>Arabic version</p>
+          </div>
+          </div>
+
+            </div>
+          </div>
+        </div>
+        {/* <!--/.Card--> */}
+
+        
+      </div>
+
+      <div className="contact">
+        <h3 className="form"> <a href="https://formsubmit.co/el/tuxucu">Let's contact us</a></h3>
+        {/* <a href="https://formsubmit.co/el/tuxucu">Contact Form</a> */}
+
+        
+      </div>
+
     </div>
   );
 }
